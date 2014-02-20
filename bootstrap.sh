bda0d2a9700242e986f766f80b7aa086b82abc2d
Mikael Peterson <mikpeterson@ebay.com>
2014-02-19 21:51:24 -0800

    created app directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@ apt-get update
 apt-get install -y apache2
 apt-get install -y postgresql postgresql-contrib
 apt-get install -y python-dev python-pip
-pip install -r /vagrant/requirements.txt
+apt-get install -y git
